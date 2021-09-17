69094e9186767035e6e693d8ef1a20c8d66416dd
Keshav Pahwa <keshavpahwa01@gmail.com>
2021-09-17 11:43:41 +0530

    Improved promote module

--- modules/promote.js
+++ modules/promote.js
@@ -15,44 +15,82 @@ module.exports = {
 
             else{
                 try{
-                    var adm = false;
-                    let parName = " "; 
-                    for (const key in BotsApp.groupMembers) {
-                        if(chat.messages.all()[0].message.extendedTextMessage.contextInfo.participant.split('@')[0] == BotsApp.groupMembers[key].id.split('@')[0]){
-                            parName = BotsApp.groupMembers[key].notify;
-                            if(BotsApp.groupMembers[key].isAdmin){
-                                adm = true;
-                                
+                    if(chat.messages.all()[0].message.extendedTextMessage.text.split("@")[1]== null){
+                    
+                        var adm = false;
+                        let parName = " "; 
+                        for (const key in BotsApp.groupMembers) {
+                            if(chat.messages.all()[0].message.extendedTextMessage.contextInfo.participant.split('@')[0] == BotsApp.groupMembers[key].id.split('@')[0]){
+                                parName = BotsApp.groupMembers[key].name;
+                                if(BotsApp.groupMembers[key].isAdmin){
+                                    adm = true;
+                                    
+                                }
                             }
                         }
+                        console.log("-------------/-------------/-------------/-------------\n");
+                        console.log(chat.messages.all()[0].message.extendedTextMessage.contextInfo.participant.split('@')[0]);
+                        console.log(parName);
+                        console.log("-------------/-------------/-------------/-------------\n");
+                    
+                        if(!adm == true){
+                            const arr =[];
+                            arr.push(chat.messages.all()[0].message.extendedTextMessage.contextInfo.participant)
+                            client.groupMakeAdmin (BotsApp.from,arr)
+                            client.sendMessage(BotsApp.from, "*" + parName + " promoted to admin*", MessageType.text);
+                        } 
+                        else{
+                            client.sendMessage(BotsApp.from, "*" + parName + " is already an admin*", MessageType.text);
+                        }
                     }
-                    console.log("-------------/-------------/-------------/-------------\n");
-                    console.log(chat.messages.all()[0].message.extendedTextMessage.contextInfo.participant.split('@')[0]);
-                    console.log(parName);
-                    console.log("-------------/-------------/-------------/-------------\n");
                 
-                    if(!adm == true){
-                        const arr =[];
-                        arr.push(chat.messages.all()[0].message.extendedTextMessage.contextInfo.participant)
-                        client.groupMakeAdmin (BotsApp.from,arr)
-                        client.sendMessage(BotsApp.from, "*" + parName + " promoted to admin*", MessageType.text);
-                    } 
                     else{
-                        client.sendMessage(BotsApp.from, "*" + parName + " is already an admin*", MessageType.text);
+                    
+                        var adm = false;
+                        let parName = " "; 
+                        for (const key in BotsApp.groupMembers) {
+                            if(chat.messages.all()[0].message.extendedTextMessage.text.split("@")[1] == BotsApp.groupMembers[key].id.split('@')[0]){
+                                parName = BotsApp.groupMembers[key].name;
+                                if(BotsApp.groupMembers[key].isAdmin){
+                                    adm = true;
+                                    
+                                }
+                            }
+                        }
+                        console.log("-------------/-------------/-------------/-------------\n");
+                        console.log(chat.messages.all()[0].message.extendedTextMessage.text.split("@")[1]);
+                        console.log(parName);
+                        console.log("-------------/-------------/-------------/-------------\n");
+                    
+                        if(!adm == true){
+                            const arr =[];
+                            arr.push(chat.messages.all()[0].message.extendedTextMessage.text.split("@")[1] + "@s.whatsapp.net")
+                            await client.groupMakeAdmin (BotsApp.from,arr)
+                            client.sendMessage(BotsApp.from, "*" + parName + " promoted to admin*", MessageType.text);
+                        } 
+                        else{
+                            client.sendMessage(BotsApp.from, "*" + parName + " is already an admin*", MessageType.text);
+                        }
                     }
                 }
                 catch(err){
-                    if(chat.messages.all()[0].message.extendedTextMessage || err instanceof TypeError){
-                        console.log(
-                            chalk.redBright.bold("Please reply to the person for promotion"));
-                        client.sendMessage(BotsApp.from, "*Please reply to someone for promotion*", MessageType.text);
+                        if(chat.messages.all()[0].message.extendedTextMessage || err instanceof TypeError){
+                            console.log(
+                                chalk.redBright.bold("Please reply to the person for promotion"));
+                            client.sendMessage(BotsApp.from, "*Please reply to someone for promotion*", MessageType.text);
+                        }
+                        else{
+                            console.log(err);
+                        }
+                
                     }
-                    else{
-                        console.log(err);
-                    }
-            
-                }
+
             }
-        }       
+                
+        }
     }
 }
+    
+           
+
+
