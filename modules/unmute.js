8bc12a29fdd27bcedc10adf8f27a33f81ef55d58
Johann Jose <72455289+j0h4nn1410@users.noreply.github.com>
2021-09-20 16:55:10 +0300

    Updated unmute.js with better formatting.

--- modules/unmute.js
+++ modules/unmute.js
@@ -1,5 +1,4 @@
 const { GroupSettingChange, MessageType } = require('@adiwajshing/baileys');
-const chalk  = require('chalk');
 
 module.exports = {
     name: 'unmute',
@@ -7,14 +6,14 @@ module.exports = {
     extendedDescription: 'Unmute non-admin members of the group.',
     async handle(client, chat, BotsApp, args) {
         if(!BotsApp.isGroup) {
-            client.sendMessage(BotsApp.from, "*.unmute*  ```command is only applicable for a group chat.```", MessageType.text)
+            client.sendMessage(BotsApp.from, "*.unmute*  ```command is only applicable for a group chat.```", MessageType.text);
             return;
         }
         if(!BotsApp.isBotGroupAdmin) {
-            client.sendMessage(BotsApp.from, "Sorry, dont have permissions to do so.", MessageType.text)
+            client.sendMessage(BotsApp.from, "```Sorry, dont have the permissions to do so since I am not an admin.```", MessageType.text);
             return;
         }
-        client.groupSettingChange(BotsApp.from, GroupSettingChange.messageSend, false)
-        client.sendMessage(BotsApp.from, "```Chat permissions changed to```  *all group members*.", MessageType.text)
+        client.groupSettingChange(BotsApp.from, GroupSettingChange.messageSend, false);
+        client.sendMessage(BotsApp.from, "```Chat permissions changed to```  *all group members*.", MessageType.text);
     }
-}
+};
