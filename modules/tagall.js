536a77886f32546a6b8e49077f329402413ab39f
Mohit Singh Rana <mohitsinghrana_co20b5_18@dtu.ac.in>
2021-09-22 21:48:35 +0530

    Tag all Members
    Signed-off-by: Mohit Singh Rana <mohitsinghrana_co20b5_18@dtu.ac.in>

---
+++ modules/tagall.js
@@ -0,0 +1,42 @@
+const {
+    MessageType
+} = require('@adiwajshing/baileys');
+
+module.exports = {
+    name: 'tagall',
+    description: "Tag all",
+    extendedDescription: "Tag all members of the group.",
+    async handle(client, chat, BotsApp, args) {
+        if (!BotsApp.isGroup) {
+            client.sendMessage(BotsApp.from, "*.tagall*  ```command is only applicable for group chats.```", MessageType.text);
+            return;
+        }
+        let members = [];
+        for (var i = 0; i < BotsApp.groupMembers.length; i++) {
+            members[i] = BotsApp.groupMembers[i].jid;
+        }
+        if (BotsApp.isReply) {
+            client.sendMessage(BotsApp.from, chat.messages.all()[0].message.extendedTextMessage.contextInfo.quotedMessage.conversation, MessageType.text, {
+                contextInfo: {
+                    mentionedJid: members
+                }
+            });
+            return;
+        }
+        if (BotsApp.body.length > ".tagall".length) {
+            client.sendMessage(BotsApp.from, BotsApp.body.substring(7), MessageType.text, {
+                contextInfo: {
+                    mentionedJid: members
+                }
+            });
+            return;
+        }
+
+        client.sendMessage(BotsApp.from, "*Everyone!*", MessageType.text, {
+            contextInfo: {
+                mentionedJid: members
+            }
+        });
+        return;
+    }
+}
