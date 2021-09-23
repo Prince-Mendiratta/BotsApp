c996f078a41c706fb2e18a6553760cb358a5a76f
Prince Mendiratta <prince.mendi@gmail.com>
2021-09-23 20:55:12 +0530

    Update Strings DB
    Signed-off-by: Prince Mendiratta <prince.mendi@gmail.com>

--- modules/id.js
+++ modules/id.js
@@ -1,12 +1,12 @@
 const { MessageType } = require("@adiwajshing/baileys")
-const Reply =require("../lib/db.js").id;
-
+const Strings = require("../lib/db")
+const ID = Strings.add
 
 module.exports = {
     name: "alive",
-    description: Reply.DESCRIPTION,
-    extendedDescription: Reply.EXTENDED_DESCRIPTION,
+    description: ID.DESCRIPTION,
+    extendedDescription: ID.EXTENDED_DESCRIPTION,
     async handle(client, chat, BotsApp, args){
-        client.sendMessage(BotsApp.from, Reply.ALIVE_MESSAGE, MessageType.text);
+        client.sendMessage(BotsApp.from, ID.EXTENDED_DESCRIPTION, MessageType.text);
     }
 }
