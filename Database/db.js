6ec41f1329cdd04b172d67a6e600828e1d11db1b
Prashant-singla <prashantsingla02@gmail.com>
2021-09-23 13:28:01 +0530

    Modify db.js file
    Signed-off-by: Prashant-singla <prashantsingla02@gmail.com>

---
+++ Database/db.js
@@ -0,0 +1,18 @@
+const data = {
+    block: {
+        DESCRIPTION: "block contact",
+        EXTENDED_DESCRIPTION: "Add number to the blocklist.",
+        NUMBER_SYNTAX_ERROR:
+            "Enter a valid contact number. Valid syntax,\n    1. XXXXXXXXXX\n    2. Tag the person\n    3. +YYXXXXXXXXXX (YY- Country Code, without zeros)",
+        MESSAGE_NOT_TAGGED: "Tag a message or enter a number",
+    },
+    unblock: {
+        DESCRIPTION: "Unblock contact",
+        EXTENDED_DESCRIPTION: "Remove number from the blocklist.",
+        NUMBER_SYNTAX_ERROR:
+            "Enter a valid contact number. Valid syntax,\n    1. XXXXXXXXXX\n    2. Tag the person\n    3. +YYXXXXXXXXXX (YY- Country Code, without zeros)",
+        MESSAGE_NOT_TAGGED: "Tag a message or enter a number",
+    },
+};
+
+module.exports = data;
