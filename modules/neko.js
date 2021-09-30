30885958569cf5fa3e9125990377a4fc59a4c088
Prince Mendiratta <prince.mendiratta@getastra.com>
2021-09-30 15:07:08 +0530

    Add processing message
    Signed-off-by: Prince Mendiratta <prince.mendiratta@getastra.com>

--- modules/neko.js
+++ modules/neko.js
@@ -2,8 +2,8 @@ const { MessageType } = require("@adiwajshing/baileys");
 const chalk = require("chalk");
 const number = require("../sidekick/input-sanitization");
 const String = require("../lib/db.js");
-const REPLY = String.neko;
 const got = require("got");
+const REPLY = String.neko;
 module.exports = {
   name: "neko",
   description: REPLY.DESCRIPTION,
@@ -13,6 +13,7 @@ module.exports = {
         await client.sendMessage(BotsApp.chatId, REPLY.ENTER_TEXT, MessageType.text);
         return;
     }
+    const proccessing = await client.sendMessage(BotsApp.chatId, REPLY.PROCESSING, MessageType.text);
     try {
         var json = {
             content: args.join(" ")
@@ -25,15 +26,17 @@ module.exports = {
         console.log(json.result.key);
         console.log("---------------------------------------------");
         neko_url = "https://nekobin.com/" + json.result.key;
-        await client.sendMessage(BotsApp.chatId, neko_url, MessageType.text);
+        client.sendMessage(BotsApp.chatId, neko_url, MessageType.text);
+        return await client.deleteMessage (BotsApp.chatId, {id: proccessing.key.id, remoteJid: BotsApp.chatId, fromMe: true});
     } catch (err) {
         if(json.result == undefined){
-            client.sendMessage(BotsApp.chatId, REPLY.TRY_LATER, MessageType.text)
+            client.sendMessage(BotsApp.chatId, REPLY.TRY_LATER, MessageType.text);
         }
         else{
             await client.sendMessage(BotsApp.chatId, "ERROR", MessageType.text);
             console.log(err);
         }
+        return await client.deleteMessage (BotsApp.chatId, {id: proccessing.key.id, remoteJid: BotsApp.chatId, fromMe: true});
     }
   },
 };
