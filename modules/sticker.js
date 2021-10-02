2cc6b22b0f83dd6a4384d92c9eeee0412f9bf550
Prashant-singla <prashantsingla02@gmail.com>
2021-10-02 10:49:31 +0530

    Add execution time functionality and video trim support
    Signed-off-by: Prashant-singla <prashantsingla02@gmail.com>

--- modules/sticker.js
+++ modules/sticker.js
@@ -1,9 +1,11 @@
 const { MessageType, MimetypeMap } = require("@adiwajshing/baileys");
 const ffmpeg = require("fluent-ffmpeg");
 const fs = require("fs");
-const inputSanitization = require("../../../BotsApp - Copy/sidekick/input-sanitization")
+const inputSanitization = require("../sidekick/input-sanitization");
+const { JSDOM } = require("jsdom");
+const { window } = new JSDOM();
 
-const Strings = require("../../../BotsApp - Copy/lib/db");
+const Strings = require("../lib/db");
 const STICKER = Strings.sticker;
 
 module.exports = {
@@ -11,30 +13,26 @@ module.exports = {
     description: STICKER.DESCRIPTION,
     extendedDescription: STICKER.EXTENDED_DESCRIPTION,
     async handle(client, chat, BotsApp, args) {
-        if (
-            BotsApp.isReplyImage ||
-            BotsApp.isReplyGIF ||
-            BotsApp.isReplyVideo
-        ) {
+        // Task starts here
+        var startTime = window.performance.now();
+
+        // Function to convert media to sticker
+        const convertToSticker = async (imageId , replyChat) => {
             var downloading = await client.sendMessage(
                 BotsApp.chatId,
                 STICKER.DOWNLOADING,
                 MessageType.text
             );
+
             
-            var imageId = chat.message.extendedTextMessage.contextInfo.stanzaId;
-            console.log("repliedImageMessageId --> " + imageId);
-            var replyChat = {
-                message:
-                    chat.message.extendedTextMessage.contextInfo.quotedMessage,
-            };
+
             const fileName = "./tmp/convert_to_sticker-" + imageId;
             const filePath = await client.downloadAndSaveMediaMessage(
                 replyChat,
                 fileName
             );
             const stickerPath = "./tmp/st-" + imageId + ".webp";
-            if (BotsApp.isReplyVideo === false && BotsApp.isReplyImage) {
+            if (BotsApp.type === "image" || BotsApp.isReplyImage) {
                 await ffmpeg(filePath)
                     .outputOptions(["-y", "-vcodec libwebp"])
                     .videoFilters(
@@ -47,37 +45,8 @@ module.exports = {
                             fs.readFileSync(stickerPath),
                             MessageType.sticker
                         );
-                        inputSanitization.deleteFiles(filePath , stickerPath);
-                        return await client.deleteMessage(BotsApp.chatId, {
-                            id: downloading.key.id,
-                            remoteJid: BotsApp.chatId,
-                            fromMe: true,
-                        });
-                    });
-            } else {
-                await ffmpeg(filePath)
-                    .outputOptions([
-                        "-y",
-                        "-vcodec libwebp",
-                        "-lossless 1",
-                        "-qscale 1",
-                        "-preset default",
-                        "-loop 0",
-                        "-an",
-                        "-vsync 0",
-                        "-s 600x600",
-                    ])
-                    .videoFilters(
-                        "scale=600:600:flags=lanczos:force_original_aspect_ratio=decrease,format=rgba,pad=600:600:(ow-iw)/2:(oh-ih)/2:color=#00000000,setsar=1"
-                    )
-                    .save(stickerPath)
-                    .on("end", async () => {
-                        await client.sendMessage(
-                            BotsApp.chatId,
-                            fs.readFileSync(stickerPath),
-                            MessageType.sticker
-                        );
-                        inputSanitization.deleteFiles(filePath , stickerPath);
+                        inputSanitization.deleteFiles(filePath, stickerPath);
+                        inputSanitization.performanceTime(startTime);
                         return await client.deleteMessage(BotsApp.chatId, {
                             id: downloading.key.id,
                             remoteJid: BotsApp.chatId,
@@ -85,29 +54,69 @@ module.exports = {
                         });
                     });
             }
+            await ffmpeg(filePath)
+                .duration(8)
+                .outputOptions([
+                    "-y",
+                    "-vcodec libwebp",
+                    "-lossless 1",
+                    "-qscale 1",
+                    "-preset default",
+                    "-loop 0",
+                    "-an",
+                    "-vsync 0",
+                    "-s 600x600",
+                ])
+                .videoFilters(
+                    "scale=600:600:flags=lanczos:force_original_aspect_ratio=decrease,format=rgba,pad=600:600:(ow-iw)/2:(oh-ih)/2:color=#00000000,setsar=1"
+                )
+                .save(stickerPath)
+                .on("end", async () => {
+                    await client.sendMessage(
+                        BotsApp.chatId,
+                        fs.readFileSync(stickerPath),
+                        MessageType.sticker
+                    );
+                    inputSanitization.deleteFiles(filePath, stickerPath);
+                    inputSanitization.performanceTime(startTime);
+                    return await client.deleteMessage(BotsApp.chatId, {
+                        id: downloading.key.id,
+                        remoteJid: BotsApp.chatId,
+                        fromMe: true,
+                    });
+                });
+        };
 
-            
-            // fs.unlink(filePath, (err) => {
-            //     if (err) console.log(err);
-            //     else {
-            //         console.log("\nDeleted file at: " + filePath);
-            //     }
-            // });
-            // fs.unlink(stickerPath, (err) => {
-            //     if (err) console.log(err);
-            //     else {
-            //         console.log("\nDeleted file at: " + stickerPath);
-            //     }
-            // });
-            
-            
+        // User sends media message along with command in caption
+        if (BotsApp.isImage || BotsApp.isGIF || BotsApp.isVideo) {
+            var replyChatObject = {
+                message: chat.message,
+            };
+            var imageId = chat.key.id;
+            console.log("repliedImageMessageId --> " + imageId);
+            convertToSticker(imageId , replyChatObject);
+        }
+        // Replied to an image , gif or video
+        else if (
+            BotsApp.isReplyImage ||
+            BotsApp.isReplyGIF ||
+            BotsApp.isReplyVideo
+        ) {
+            var replyChatObject = {
+                message:
+                    chat.message.extendedTextMessage.contextInfo.quotedMessage,
+            };
+            var imageId = chat.message.extendedTextMessage.contextInfo.stanzaId;
+            console.log("repliedImageMessageId --> " + imageId);
+            convertToSticker(imageId , replyChatObject);
         } else {
             client.sendMessage(
                 BotsApp.chatId,
                 STICKER.TAG_A_VALID_IMAGE_MESSAGE,
                 MessageType.text
             );
-            return;
+            inputSanitization.performanceTime(startTime);
         }
+        return;
     },
 };
--- sidekick/input-sanitization.js
+++ sidekick/input-sanitization.js
@@ -1,49 +1,49 @@
 const { MessageType } = require("@adiwajshing/baileys");
-const config = require('../config')
 const fs = require("fs");
+const { JSDOM } = require("jsdom");
+const { window } = new JSDOM();
 
-exports.getCleanedContact = async (args,client,BotsApp) => {
-    var jidNumber = '';
-    var countryCode = config.COUNTRY_CODE; 
+exports.getCleanedContact = async (args, client, BotsApp) => {
+    var jidNumber = "";
+    var countryCode = "";
     if (isNaN(args[0]) || args[0][0] === "+") {
         if (args[0][0] === "@" || args[0][0] === "+") {
             jidNumber = args[0].substring(1, args[0].length + 1);
-           
-            
-        }
-        else {
-            client.sendMessage(BotsApp.chatId,"*Enter valid contact number.* Approved Syntax:\n```1. XXXXXXXXXX``` \n```2. Tag the person``` \n```3. +(YYY)XXXXXXXXXX.``` \n_(YY- Country Code, without zeros)_",MessageType.text);
+            countryCode = BotsApp.owner.substring(-28, -26);
+            if (jidNumber.length < 10 || jidNumber.length > 13) {
+                client.sendMessage(
+                    BotsApp.chatId,
+                    "*Enter valid contact number.* Approved Syntax:\n```1. XXXXXXXXXX``` \n```2. Tag the person``` \n```3. +(YYY)XXXXXXXXXX.``` \n_(YY- Country Code, without zeros)_",
+                    MessageType.text
+                );
+                return;
+            } else if (jidNumber.length === 10) {
+                jidNumber = BotsApp.owner.substring(-28, -26) + jidNumber;
+            }
+        } else {
+            client.sendMessage(
+                BotsApp.chatId,
+                "*Enter valid contact number.* Approved Syntax:\n```1. XXXXXXXXXX``` \n```2. Tag the person``` \n```3. +(YYY)XXXXXXXXXX.``` \n_(YY- Country Code, without zeros)_",
+                MessageType.text
+            );
             return;
         }
     } else {
-        jidNumber = args[0];       
+        jidNumber = args[0];
     }
-
-    if (jidNumber.length < 8 || jidNumber.length > 13) {
+    if (jidNumber.length < 10 || jidNumber.length > 13) {
         client.sendMessage(
             BotsApp.chatId,
             "*Enter valid contact number.* Approved Syntax:\n```1. XXXXXXXXXX``` \n```2. Tag the person``` \n```3. +(YYY)XXXXXXXXXX.``` \n_(YY- Country Code, without zeros)_",
             MessageType.text
         );
         return;
-    } 
-    else if (jidNumber.length === 10) { 
-        jidNumber = countryCode + jidNumber;
+    } else if (jidNumber.length === 10) {
+        jidNumber = BotsApp.owner.substring(-28, -26) + jidNumber;
     }
-    var isOnWhatsApp = await client.isOnWhatsApp(jidNumber);
-    console.log("--------------");
-    console.log(jidNumber);
-    console.log("--------------");
-    console.log(isOnWhatsApp);
-    if(isOnWhatsApp === undefined){
-        console.log("throwing error");
-        throw "NumberInvalid"; 
-    }
-    
-    // isOnWhatsApp is not working
-    return jidNumber;
-}
 
+    return jidNumber;
+};
 
 exports.deleteFiles = async (...locations) => {
     for (location of locations) {
@@ -55,3 +55,12 @@ exports.deleteFiles = async (...locations) => {
         });
     }
 };
+
+exports.performanceTime = async (startTime) => {
+    var endTime = window.performance.now();
+    console.log(
+        `-----------\nExecution time: ${
+            (endTime - startTime) / 1000
+        } seconds\n----------`
+    );
+}
