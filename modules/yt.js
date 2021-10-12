f4633e4807bd14a6a4628437df8881bea43cfae2
Keshav Pahwa <keshavpahwa01@gmail.com>
2021-10-12 19:19:53 +0530

    Updated the code for yt.js module
    Signed-off-by: Keshav Pahwa <keshavpahwa01@gmail.com>

--- modules/yt.js
+++ modules/yt.js
@@ -9,7 +9,6 @@ module.exports = {
     extendedDescription: YT.EXTENDED_DESCRIPTION,
     async handle(client, chat, BotsApp, args){
         const keyword = await yts(args.join(" "))
-
         const videos = keyword.videos.slice(0, 10);
         var topRequests = "";
         var num = 1;
@@ -20,6 +19,16 @@ module.exports = {
             topRequests = topRequests +  `*${num}.)* ${ links.title } (${ links.timestamp }) | *${ links.author.name }* | ${ links.url }\n\n`
             num++;
         } )
+
+        if(topRequests ===""){
+            client.sendMessage(BotsApp.chatId, YT.NO_VIDEOS, MessageType.text);
+            await client.deleteMessage(BotsApp.chatId, {
+                id: reply.key.id,
+                remoteJid: BotsApp.chatId,
+                fromMe: true,
+            });
+            return;
+        }
 
         client.sendMessage(BotsApp.chatId, topRequests, MessageType.text);
         await client.deleteMessage(BotsApp.chatId, {
