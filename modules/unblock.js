cfc222ee1f0959139074159001d75a23d2ed0a8e
Keshav Pahwa <keshavpahwa01@gmail.com>
2021-10-21 18:14:54 +0530

    Dealt corner cases in block and unblock module
    Signed-off-by: Keshav Pahwa <keshavpahwa01@gmail.com>

--- modules/unblock.js
+++ modules/unblock.js
@@ -9,19 +9,45 @@ module.exports = {
     async handle(client, chat, BotsApp, args) {
         var JID = "";
         var jidNumber;
-    
-        if (args.length > 0) {
-            if (isNaN(args[0]) || args[0][0] === "+") {
-                if (args[0][0] === "@" || args[0][0] === "+") {
-                    jidNumber = args[0].substring(1, args[0].length + 1);
+        if(BotsApp.replyParticipant === BotsApp.owner){
+            client.sendMessage(
+                BotsApp.chatId,
+                "Bot can not unblock itself",
+                MessageType.text
+            );
+            return;
+        }
+        try{
+            if (args.length > 0) {
+                if (isNaN(args[0]) || args[0][0] === "+") {
+                    if (args[0][0] === "@" || args[0][0] === "+") {
+                        jidNumber = args[0].substring(1, args[0].length + 1);
+                    } else {
+        
+                        client.sendMessage(
+                            BotsApp.chatId,
+                            Reply.NUMBER_SYNTAX_ERROR, MessageType.text
+                        );
+                        return;
+                    }
                 } else {
-    
+                    jidNumber = args[0];
+                }
+                if (jidNumber.length < 10 || jidNumber.length > 13) {
                     client.sendMessage(
                         BotsApp.chatId,
+<<<<<<< HEAD
                         Reply.NUMBER_SYNTAX_ERROR, MessageType.text
+=======
+                        Reply.NUMBER_SYNTAX_ERROR,
+                        MessageType.text
+>>>>>>> Dealt corner cases in block and unblock module
                     );
                     return;
+                } else if (jidNumber.length === 10) {
+                    jidNumber = "91" + jidNumber;
                 }
+<<<<<<< HEAD
             } else {
                 jidNumber = args[0];
             }
@@ -53,21 +79,47 @@ module.exports = {
                     MessageType.text
                 );
                 return;
+=======
+                JID = jidNumber + "@s.whatsapp.net";
+            } else if (!BotsApp.isGroup) {
+                JID = BotsApp.chatId;
+                jidNumber = JID.substring(0, JID.indexOf("@"));
+            } else {
+                if (BotsApp.isReply) {
+                    JID = BotsApp.replyParticipant;
+        
+                    jidNumber = JID.substring(0, JID.indexOf("@"));
+                } else {
+                    client.sendMessage(
+                        BotsApp.chatId,
+                        Reply.MESSAGE_NOT_TAGGED,
+                        MessageType.text
+                    );
+                    return;
+                }
+>>>>>>> Dealt corner cases in block and unblock module
             }
+        
+            client.blockUser(JID, "remove");
+            console.log(
+                "Command to remove " +
+                " " +
+                jidNumber +
+                " from blockList successfully executed"
+            );
+        
+            client.sendMessage(
+                BotsApp.chatId,
+                "*" + jidNumber + " unblocked successfully.*",
+                MessageType.text
+            );
+
+        }catch(err){
+            client.sendMessage(
+                BotsApp.chatId,
+                Reply.MESSAGE_NOT_TAGGED,
+                MessageType.text
+            );
         }
-    
-        client.blockUser(JID, "remove");
-        console.log(
-            "Command to remove " +
-            " " +
-            jidNumber +
-            " from blockList successfully executed"
-        );
-    
-        client.sendMessage(
-            BotsApp.chatId,
-            "```" + jidNumber + " has been unblocked successfully.```",
-            MessageType.text
-        );
     },
 };
