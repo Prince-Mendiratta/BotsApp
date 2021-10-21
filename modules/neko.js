28cf99799844ecc0259789ce20eb81d2b02d0e20
Keshav Pahwa <keshavpahwa01@gmail.com>
2021-10-21 18:44:44 +0530

    Dealt corner cases in neko module
    Signed-off-by: Keshav Pahwa <keshavpahwa01@gmail.com>

--- modules/neko.js
+++ modules/neko.js
@@ -1,6 +1,5 @@
 const { MessageType } = require("@adiwajshing/baileys");
 const chalk = require("chalk");
-const number = require("../sidekick/input-sanitization");
 const String = require("../lib/db.js");
 const got = require("got");
 const REPLY = String.neko;
@@ -10,14 +9,23 @@ module.exports = {
   extendedDescription: REPLY.EXTENDED_DESCRIPTION,
   demo: {isEnabled: true, text: '.neko #include <iostream> \nint main() \n{\n   std::cout << "Hello BotsApp!"; \n   return 0;\n}'},
   async handle(client, chat, BotsApp, args) {
-    if(args[0] == null){
+    if(args.length === 0 && !BotsApp.isReply){
         await client.sendMessage(BotsApp.chatId, REPLY.ENTER_TEXT, MessageType.text);
         return;
     }
-    const proccessing = await client.sendMessage(BotsApp.chatId, REPLY.PROCESSING, MessageType.text);
+    console.log(args.length)
+    console.log(args)
+    const processing = await client.sendMessage(BotsApp.chatId, REPLY.PROCESSING, MessageType.text);
     try {
-        var json = {
-            content: BotsApp.body.replace(BotsApp.body[0] + BotsApp.commandName + " ", "")
+        if(!BotsApp.isReply){
+            var json = {
+                content: BotsApp.body.replace(BotsApp.body[0] + BotsApp.commandName + " ", "")
+            }
+        }
+        else{
+            var json = {
+                content: BotsApp.replyMessage.replace(BotsApp.body[0] + BotsApp.commandName + " ", "")
+            }
         }
         let text = await got.post("https://nekobin.com/api/documents", {
             json
@@ -28,7 +36,7 @@ module.exports = {
         console.log("---------------------------------------------");
         neko_url = "https://nekobin.com/" + json.result.key;
         client.sendMessage(BotsApp.chatId, neko_url, MessageType.text);
-        return await client.deleteMessage (BotsApp.chatId, {id: proccessing.key.id, remoteJid: BotsApp.chatId, fromMe: true});
+        return await client.deleteMessage (BotsApp.chatId, {id: processing.key.id, remoteJid: BotsApp.chatId, fromMe: true});
     } catch (err) {
         if(json.result == undefined){
             client.sendMessage(BotsApp.chatId, REPLY.TRY_LATER, MessageType.text);
@@ -37,7 +45,7 @@ module.exports = {
             await client.sendMessage(BotsApp.chatId, "```Woops, something went wrong. Try again later.```", MessageType.text);
             console.log(err);
         }
-        return await client.deleteMessage (BotsApp.chatId, {id: proccessing.key.id, remoteJid: BotsApp.chatId, fromMe: true});
+        return await client.deleteMessage (BotsApp.chatId, {id: processing.key.id, remoteJid: BotsApp.chatId, fromMe: true});
     }
   },
 };
