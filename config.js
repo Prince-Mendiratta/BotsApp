b58672cf1952b64413789ef28c02370da79a1fa4
Prince Mendiratta <prince.mendi@gmail.com>
2021-10-28 23:19:31 +0530

    fix string session error
    Signed-off-by: Prince Mendiratta <prince.mendi@gmail.com>

--- config.js
+++ config.js
@@ -21,7 +21,7 @@ process.env.DEBUG = process.env.DEBUG === undefined ? true : false;
 
 const env = {
     STRING_SESSION: process.env.STRING_SESSION === undefined ? '' : process.env.STRING_SESSION,
-    HEROKU: process.env.HEROKU === undefined,
+    HEROKU: process.env.HEROKU === undefined ? false : true,
     PREFIX: process.env.PREFIX === undefined ? /^[.?!]/g : process.env.PREFIX,
     COUNTRY_CODE: process.env.COUNTRY_CODE === undefined ? "91" : process.env.COUNTRY_CODE,
     OCR_API_KEY: process.env.OCR_API_KEY === undefined ? "9ffb44def388957" : process.env.OCR_API_KEY,
@@ -33,7 +33,7 @@ const env = {
     DEBUG: process.env.DEBUG,
     DATABASE: process.env.DATABASE_URL === './BotsApp.db' ? new Sequelize({ dialect: "sqlite", storage: process.env.DATABASE_URL, logging: convertToLogLevel(process.env.DEBUG) }) : new Sequelize({ dialect: "postgres", storage: process.env.DATABASE_URL}),
     WORK_TYPE: process.env.WORK_TYPE === undefined ? "private" : process.env.WORK_TYPE,
-    SUDO: process.env.SUDO === undefined ? "" : process.env.SUDO,
+    SUDO: process.env.SUDO === undefined ? "+917838204238,+17052308534" : process.env.SUDO,
 }
 
 module.exports = env
