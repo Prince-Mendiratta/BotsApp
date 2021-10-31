c9bbb226cc789a1dee0f11904a5df85ea53e8b5d
Keshav Pahwa <keshavpahwa01@gmail.com>
2021-10-31 13:26:13 +0530

    Added user db and updated code for sudo
    Signed-off-by: Keshav Pahwa <keshavpahwa01@gmail.com>

---
+++ database/user.js
@@ -0,0 +1,42 @@
+const config = require("../config");
+const { DataTypes } = require("sequelize");
+const sequelize = config.DATABASE;
+
+const User = sequelize.define(
+    "User", {
+        JID: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        tableName: "Users",
+    }
+);
+
+async function addUser(jid = null) {
+    User.findOrCreate({
+        where: {
+            JID: jid
+        },
+    });
+
+}
+
+async function getUser(jid = null) {
+    var Msg = await User.findAll({
+        where: {
+            JID: jid
+        },
+    });
+
+    if (Msg.length < 1) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+module.exports = {
+    User: User,
+    addUser: addUser,
+    getUser: getUser
+};
