f396fa40625b68dfee28966091e627cb4545122e
Keshav Pahwa <keshavpahwa01@gmail.com>
2021-11-30 12:55:04 +0530

    Added try catch and strings for create module
    Signed-off-by: Keshav Pahwa <keshavpahwa01@gmail.com>

--- modules/create.js
+++ modules/create.js
@@ -2,7 +2,7 @@ const { MessageType, Mimetype } = require("@adiwajshing/baileys");
 const chalk = require("chalk");
 const inputSanitization = require("../sidekick/input-sanitization");
 const strings = require("../lib/db")
-const CREATE = strings.CREATE;
+const CREATE = strings.create;
 
 module.exports = {
     name: "create",
@@ -10,29 +10,37 @@ module.exports = {
     extendedDescription: CREATE.EXTENDED_DESCRIPTION,
     demo: { isEnabled: false },
     async handle(client, chat, BotsApp, args) {
-        if(args.length === 0) {
-            client.sendMessage(BotsApp.chatId, "```Enter the name of the grp```", MessageType.text);
-            return;
-        }
-        let nameOfTheGrp = 
-        BotsApp.body.replace(
-            BotsApp.body[0] + BotsApp.commandName + " ",
-            ""
-        );
-
-        if(BotsApp.isPm) {
-            const group = await client.groupCreate (nameOfTheGrp, [BotsApp.owner, BotsApp.sender]);
-            return;
-        }
-        else {
-            if(BotsApp.isReply){
-                    const group = await client.groupCreate (nameOfTheGrp, [BotsApp.sender, BotsApp.replyParticipant]);
-                    return;
-            }
-            else {
-                client.sendMessage(BotsApp.chatId, "```Tag a person that should be included in group```", MessageType.text);
+        try{
+            if(args.length === 0) {
+                client.sendMessage(BotsApp.chatId, CREATE.NO_TEXT, MessageType.text);
                 return;
             }
+            let nameOfTheGrp = 
+            BotsApp.body.replace(
+                BotsApp.body[0] + BotsApp.commandName + " ",
+                ""
+            );
+
+            if(BotsApp.isPm) {
+                const group = await client.groupCreate (nameOfTheGrp, [BotsApp.owner, BotsApp.sender]);
+                client.sendMessage(BotsApp.chatId, CREATE.GROUP_CREATED, MessageType.text);
+                return;
+            }
+            else {
+                if(BotsApp.isReply){
+                        const group = await client.groupCreate (nameOfTheGrp, [BotsApp.sender, BotsApp.replyParticipant]);
+                        client.sendMessage(BotsApp.chatId, CREATE.GROUP_CREATED, MessageType.text);
+                        return;
+                }
+                else {
+                    client.sendMessage(BotsApp.chatId, CREATE.TAG_PERSON, MessageType.text);
+                    return;
+                }
+            }
+        }
+
+        catch(err) {
+            await inputSanitization.handleError(err, client, BotsApp);
         }
     }
 }
