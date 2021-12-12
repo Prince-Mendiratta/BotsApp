0edd1094c22a7b642c1ad7748069a7977c5e66cb
Prince Mendiratta <prince.mendi@gmail.com>
2021-12-12 17:31:30 +0530

    disable private acknowledgement

--- core/clearance.js
+++ core/clearance.js
@@ -76,29 +76,29 @@ const clearance = async(BotsApp, client, isBlacklist) => {
                     `not executed in private Work Type.`
                 )
             );
-            var messageSent = await Users.getUser(BotsApp.chatId);
-            if(messageSent){
-                console.log(chalk.blueBright.bold("[INFO] Promo message had already been sent to " + BotsApp.chatId))
-                return false;
-            }
-            else{
-                await client.sendMessage(
-                    BotsApp.chatId,
-                    GENERAL.SUDO_PERMISSION.format({ worktype: "private", groupName: BotsApp.groupName ? BotsApp.groupName : "private chat", commandName: BotsApp.commandName }),
-                    MessageType.text,
-                    {
-                        contextInfo: {
-                            stanzaId: BotsApp.chatId,
-                            participant: BotsApp.sender,
-                            quotedMessage: {
-                                conversation: BotsApp.body,
-                            },
-                        },
-                    }
-                );
-                await Users.addUser(BotsApp.chatId)
-                return false;
-            }
+//             var messageSent = await Users.getUser(BotsApp.chatId);
+//             if(messageSent){
+//                 console.log(chalk.blueBright.bold("[INFO] Promo message had already been sent to " + BotsApp.chatId))
+//                 return false;
+//             }
+//             else{
+//                 await client.sendMessage(
+//                     BotsApp.chatId,
+//                     GENERAL.SUDO_PERMISSION.format({ worktype: "private", groupName: BotsApp.groupName ? BotsApp.groupName : "private chat", commandName: BotsApp.commandName }),
+//                     MessageType.text,
+//                     {
+//                         contextInfo: {
+//                             stanzaId: BotsApp.chatId,
+//                             participant: BotsApp.sender,
+//                             quotedMessage: {
+//                                 conversation: BotsApp.body,
+//                             },
+//                         },
+//                     }
+//                 );
+//                 await Users.addUser(BotsApp.chatId)
+//                 return false;
+//             }
         }
     }else{
         return true;
