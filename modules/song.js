ba21c01f8037de9e385d1958d1987b0307abfe74
Keshav Pahwa <keshavpahwa01@gmail.com>
2021-12-12 13:21:58 +0530

    Fixed a minor bug in song module
    Signed-off-by: Keshav Pahwa <keshavpahwa01@gmail.com>

--- modules/song.js
+++ modules/song.js
@@ -42,16 +42,11 @@ module.exports = {
             var startTime = window.performance.now();
             var Id = " ";
             if (args[0].includes("youtu")) {
-                Id = args[0];
-                try {
-                    if (args[0].includes("watch?v=")) {
-                        var songId = args[0].split("watch?v=")[1];
-                    } else {
-                        var songId = args[0].split("/")[3];
-                    }
-                    const video = await yts({ videoId: songId });
-                } catch (err) {
-                    throw err;
+                if(ytdl.validateURL(args[0])){
+                    Id = args[0];
+                }
+                else{
+                    return;
                 }
             } else {
                 var song = await yts(args.join(" "));
