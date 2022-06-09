455c04a07c90c6c792dc2ca9f3bdeeb27aadc724
Prince Mendiratta <prince.mendi@gmail.com>
2022-06-09 23:07:49 +0530

    indentation changes
    Signed-off-by: Prince Mendiratta <prince.mendi@gmail.com>

--- modules/news.ts
+++ modules/news.ts
@@ -1,5 +1,4 @@
 import STRINGS from "../lib/db.js";
-const NEWS = STRINGS.news;
 import Client from "../sidekick/client";
 import { proto } from "@adiwajshing/baileys";
 import BotsApp from "../sidekick/sidekick";
@@ -7,134 +6,134 @@ import { MessageType } from "../sidekick/message-type";
 import inputSanitization from "../sidekick/input-sanitization";
 import Axios from "axios";
 import config from "../config";
+const NEWS = STRINGS.news;
 
 module.exports = {
-	name: "news",
-	description: NEWS.DESCRIPTION,
-	extendedDescription: NEWS.EXTENDED_DESCRIPTION,
-	demo: { isEnabled: false },
+    name: "news",
+    description: NEWS.DESCRIPTION,
+    extendedDescription: NEWS.EXTENDED_DESCRIPTION,
+    demo: { isEnabled: false },
+    async handle(
+        client: Client,
+        chat: proto.IWebMessageInfo,
+        BotsApp: BotsApp,
+        args: string[]
+    ): Promise<void> {
+        /*******************************************
+         *
+         *
+         * Functions
+         *
+         *
+         ********************************************/
+        const checkPub = async (newsList: string[], requestedPub: string) => {
+            for (let i = 0; i < newsList.length; i++) {
+                if (newsList[i].toUpperCase == requestedPub.toUpperCase) {
+                    return newsList[i];
+                }
+                return;
+            }
+        };
+        /*******************************************
+         *
+         *
+         * Actions
+         *
+         *
+         ********************************************/
+        if (args.length === 0) {
+            await client
+                .sendMessage(BotsApp.chatId, NEWS.NO_COMMMAND, MessageType.text)
+                .catch((err) => inputSanitization.handleError(err, client, BotsApp));
+            return;
+        }
+        if (args[0] == "help") {
+            await client
+                .sendMessage(
+                    BotsApp.chatId,
+                    NEWS.EXTENDED_DESCRIPTION,
+                    MessageType.text
+                )
+                .catch((err) => inputSanitization.handleError(err, client, BotsApp));
+            return;
+        }
+        if (args[0] == "search") {
+            args.shift();
+            var searchTerm = args.join(" ");
+            let searchResponse;
+            try {
+                await Axios.get(
+                    config.NEWS_API_URL + "news-list?searchTerm=" + searchTerm
+                )
+                    .then((res) => {
+                        searchResponse = res.data;
+                    })
+                    .catch((error) => {
+                        console.error(error);
+                    });
+            } catch (error) {
+                throw error;
+            }
 
-	async handle(
-		client: Client,
-		chat: proto.IWebMessageInfo,
-		BotsApp: BotsApp,
-		args: string[]
-	): Promise<void> {
-		/*******************************************
-		 *
-		 *
-		 * Functions
-		 *
-		 *
-		 ********************************************/
-		const checkPub = async (newsList: string[], requestedPub: string) => {
-			for (let i = 0; i < newsList.length; i++) {
-				if (newsList[i].toUpperCase == requestedPub.toUpperCase) {
-					return newsList[i];
-				}
-				return;
-			}
-		};
-		/*******************************************
-		 *
-		 *
-		 * Actions
-		 *
-		 *
-		 ********************************************/
-		if (args.length === 0) {
-			await client
-				.sendMessage(BotsApp.chatId, NEWS.NO_COMMMAND, MessageType.text)
-				.catch((err) => inputSanitization.handleError(err, client, BotsApp));
-			return;
-		}
-		if (args[0] == "help") {
-			await client
-				.sendMessage(
-					BotsApp.chatId,
-					NEWS.EXTENDED_DESCRIPTION,
-					MessageType.text
-				)
-				.catch((err) => inputSanitization.handleError(err, client, BotsApp));
-			return;
-		}
-		if (args[0] == "search") {
-			args.shift();
-			var searchTerm = args.join(" ");
-			let searchResponse;
-			try {
-				await Axios.get(
-					config.NEWS_API_URL + "news-list?searchTerm=" + searchTerm
-				)
-					.then((res) => {
-						searchResponse = res.data;
-					})
-					.catch((error) => {
-						console.error(error);
-					});
-			} catch (error) {
-				throw error;
-			}
+            for (let i = 0; i < searchResponse.length; i++) {
+                searchResponse[i] = `${i + 1}` + ".)  " + `${searchResponse[i]}`;
+            }
 
-			for (let i = 0; i < searchResponse.length; i++) {
-				searchResponse[i] = `${i + 1}` + ".)  " + `${searchResponse[i]}`;
-			}
-
-			let message = searchResponse.join("\n\n");
-			await client
-				.sendMessage(BotsApp.chatId, message, MessageType.text)
-				.catch((err) => inputSanitization.handleError(err, client, BotsApp));
-			return;
-		}
-		if (args[0] == "fetch") {
-			args.shift();
-			var searchTerm = args.join(" ");
-			let searchResponse;
-			try {
-				await Axios.get(
-					config.NEWS_API_URL + "news-list?searchTerm=" + searchTerm
-				)
-					.then((res) => {
-						searchResponse = res.data;
-					})
-					.catch((error) => {
-						console.error(error);
-					});
-			} catch (error) {
-				throw error;
-			}
-			let foundPub = await checkPub(searchResponse, searchTerm);
-			let message =
-				"```Your requested publication``` *" +
-				foundPub +
-				"* ```is being fetched by BotsApp, this may take some time, please be patient!```";
-			if (!foundPub) {
-				message = "```Sorry, no publication found by that name!```";
-				await client
-					.sendMessage(BotsApp.chatId, message, MessageType.text)
-					.catch((err) => inputSanitization.handleError(err, client, BotsApp));
-				return;
-			}
-			await client
-				.sendMessage(
-					BotsApp.chatId,
-					{
-						url:
-							config.NEWS_API_URL + "news?pubName=" + foundPub + "&format=epub",
-					},
-					MessageType.document,
-					{
-						mimetype: "application/epub",
-						filename: foundPub + ".epub",
-						caption: message,
-					}
-				)
-				.catch((err) => inputSanitization.handleError(err, client, BotsApp));
-			message = "```Your requested publication fetched by BotsApp``` ☝️";
-			await client
-				.sendMessage(BotsApp.chatId, message, MessageType.text)
-				.catch((err) => inputSanitization.handleError(err, client, BotsApp));
-			return;
-		}
-	},
+            let message = searchResponse.join("\n\n");
+            await client
+                .sendMessage(BotsApp.chatId, message, MessageType.text)
+                .catch((err) => inputSanitization.handleError(err, client, BotsApp));
+            return;
+        }
+        if (args[0] == "fetch") {
+            args.shift();
+            var searchTerm = args.join(" ");
+            let searchResponse;
+            try {
+                await Axios.get(
+                    config.NEWS_API_URL + "news-list?searchTerm=" + searchTerm
+                )
+                    .then((res) => {
+                        searchResponse = res.data;
+                    })
+                    .catch((error) => {
+                        console.error(error);
+                    });
+            } catch (error) {
+                throw error;
+            }
+            let foundPub = await checkPub(searchResponse, searchTerm);
+            let message =
+                "```Your requested publication``` *" +
+                foundPub +
+                "* ```is being fetched by BotsApp, this may take some time, please be patient!```";
+            if (!foundPub) {
+                message = "```Sorry, no publication found by that name!```";
+                await client
+                    .sendMessage(BotsApp.chatId, message, MessageType.text)
+                    .catch((err) => inputSanitization.handleError(err, client, BotsApp));
+                return;
+            }
+            await client
+                .sendMessage(
+                    BotsApp.chatId,
+                    {
+                        url:
+                            config.NEWS_API_URL + "news?pubName=" + foundPub + "&format=epub",
+                    },
+                    MessageType.document,
+                    {
+                        mimetype: "application/epub",
+                        filename: foundPub + ".epub",
+                        caption: message,
+                    }
+                )
+                .catch((err) => inputSanitization.handleError(err, client, BotsApp));
+            message = "```Your requested publication fetched by BotsApp``` ☝️";
+            await client
+                .sendMessage(BotsApp.chatId, message, MessageType.text)
+                .catch((err) => inputSanitization.handleError(err, client, BotsApp));
+            return;
+        }
+    },
 };
