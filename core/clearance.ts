f768aed59af0879b069f13ba16b8b6fbbd805f88
_____Dy_____ <khoeunmarady@gmail.com>
2022-10-24 19:35:50 +0700

    build(deps): create 2 files and update 2 files

--- core/clearance.ts
+++ core/clearance.ts
@@ -70,7 +70,7 @@ const clearance = async (BotsApp: BotsApp, client: Client, isBlacklist: boolean)
                 } else {
                     return true;
                 }
-            }else if(BotsApp.isPm){
+            } else if (BotsApp.isPm) {
                 return true;
             }
         } else if (config.WORK_TYPE.toLowerCase() != "public" && !BotsApp.isSenderSUDO) {
@@ -81,29 +81,29 @@ const clearance = async (BotsApp: BotsApp, client: Client, isBlacklist: boolean)
                     `not executed in private Work Type.`
                 )
             );
-            //             let messageSent = await Users.getUser(BotsApp.chatId);
-            //             if(messageSent){
-            //                 console.log(chalk.blueBright.bold("[INFO] Promo message had already been sent to " + BotsApp.chatId))
-            //                 return false;
-            //             }
-            //             else{
-            //                 await client.sendMessage(
-            //                     BotsApp.chatId,
-            //                     GENERAL.SUDO_PERMISSION.format({ worktype: "private", groupName: BotsApp.groupName ? BotsApp.groupName : "private chat", commandName: BotsApp.commandName }),
-            //                     MessageType.text,
-            //                     {
-            //                         contextInfo: {
-            //                             stanzaId: BotsApp.chatId,
-            //                             participant: BotsApp.sender,
-            //                             quotedMessage: {
-            //                                 conversation: BotsApp.body,
-            //                             },
-            //                         },
-            //                     }
-            //                 );
-            //                 await Users.addUser(BotsApp.chatId)
-            //                 return false;
-            //             }
+            let messageSent = await Users.getUser(BotsApp.chatId);
+            if (messageSent) {
+                console.log(chalk.blueBright.bold("[INFO] Promo message had already been sent to " + BotsApp.chatId))
+                return false;
+            }
+            else {
+                await client.sendMessage(
+                    BotsApp.chatId,
+                    GENERAL.SUDO_PERMISSION.format({ worktype: "private", groupName: BotsApp.groupName ? BotsApp.groupName : "private chat", commandName: BotsApp.commandName }),
+                    MessageType.text,
+                    {
+                        contextInfo: {
+                            stanzaId: BotsApp.chatId,
+                            participant: BotsApp.sender,
+                            quotedMessage: {
+                                conversation: BotsApp.body,
+                            },
+                        },
+                    }
+                );
+                await Users.addUser(BotsApp.chatId)
+                return false;
+            }
         }
     } else {
         return true;
