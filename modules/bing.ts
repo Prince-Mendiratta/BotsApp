8864188ef9bf3b1ce2f4b0e4aa2865bb7ee59fbb
Kofi <kofimokome10@gmail.com>
2023-03-22 19:01:40 +0100

    Fix app crashed if bing request fails

--- modules/bing.ts
+++ modules/bing.ts
@@ -17,6 +17,7 @@ export default {
     extendedDescription: bing.EXTENDED_DESCRIPTION,
     demo: {isEnabled: true, text: ".bing"},
     async handle(client: Client, chat: proto.IWebMessageInfo, BotsApp: BotsApp, args: string[]): Promise<void> {
+        try {
         //@ts-ignore
         const sendMessageToBing = async (api: BingChat, message: string) => {
             const mes = await client.sendMessage(
@@ -51,7 +52,7 @@ export default {
             }
         }
 
-        try {
+
             console.log(BotsApp.sender, BotsApp, chat)
             if (config.BING_COOKIE.trim().length == 0) {
                 client.sendMessage(
