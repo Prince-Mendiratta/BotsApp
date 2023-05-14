9bdcbc96e19a61dcbac5e63a83d4b5d1309936a2
AlePaff <alejandropaff@gmail.com>
2023-05-13 22:26:51 -0300

    add random module given a list of values selects a random one

--- lib/db.ts
+++ lib/db.ts
@@ -240,6 +240,12 @@ const data = {
         PROCESSING: "```Generating QR code. Please wait...```",
         IMAGE_CAPTION: "```Here's your QR image.```",
     },
+    random: {
+        DESCRIPTION: "Given a list of items, pick a random one.",
+        EXTENDED_DESCRIPTION:
+            "```Choose a random value by using command``` *.random WatchTV WashDishes ...* .",
+        NO_ARG: "```Please enter at least one value to be picked. For example .random Value1 Value2```",
+    },
     rbl: {
         DESCRIPTION: "Module to enable a blacklist person or group to use the bot.",
         EXTENDED_DESCRIPTION:
